10be577edfcd58a71fe3da47d5faed006e7e2eca
Abhi <onelife1906@gmail.com>
2020-10-29 22:02:58 +0530

    Fixed Warnings

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -21,13 +21,7 @@ const mapStateToProps = state => {
 }
 
 class Main extends Component {
-
-  constructor(props) {
-    super(props);
-  }
-
-
-
+  
   render() {
     const HomePage = () => {
       return(
