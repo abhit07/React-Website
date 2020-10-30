c1c2290a2fbe3c4b6c449685824f13a2a19cc03f
Abhi <onelife1906@gmail.com>
2020-10-30 10:48:40 +0530

    Updates

---
+++ precache-manifest.7fc5189adf61525f9ec395edc4b07465.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "385b38fffaf89124c30fa4bcd2ccdcf1",
+    "url": "/React-Website/index.html"
+  },
+  {
+    "revision": "c8e1d31c31ccdd29cc7b",
+    "url": "/React-Website/static/css/2.2b94a128.chunk.css"
+  },
+  {
+    "revision": "d90e015dffff7d3d3afd",
+    "url": "/React-Website/static/css/main.9c4dcbea.chunk.css"
+  },
+  {
+    "revision": "c8e1d31c31ccdd29cc7b",
+    "url": "/React-Website/static/js/2.dcee50ee.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/React-Website/static/js/2.dcee50ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d90e015dffff7d3d3afd",
+    "url": "/React-Website/static/js/main.9a35dd89.chunk.js"
+  },
+  {
+    "revision": "f6338b2774bfde96f745",
+    "url": "/React-Website/static/js/runtime-main.0f4fc386.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/React-Website/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/React-Website/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/React-Website/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/React-Website/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/React-Website/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
